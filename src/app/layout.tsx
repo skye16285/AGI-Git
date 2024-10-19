322acc9773bbdcbafe494fec83a5679a04a756e1
skye16285 <skye16285@gmail.com>
2024-10-19 20:11:16 +0800

    feat: layout, homepage, WagmiProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
+import { Providers } from "./component/provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "",
-  description: "",
+  title: "AGI GIT",
+  description: "Facilitating seamless collaboration in AGI training",
 };
 
 export default function RootLayout({
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
-import { useEffect, useRef,  } from "react";
+import { useEffect, useRef, useState } from "react";
 import { AppContext } from "./component/wallet/appContext";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
+import { Github } from "lucide-react";
 
 export default function Page() {
     const router = useRouter();
@@ -16,7 +18,7 @@ export default function Page() {
     }, []);
 
     const { isConnected } = useAccount();
-
+    console.log({ isConnected });
     useEffect(() => {
         if (isConnected) {
             void router.push("/home");
@@ -28,6 +30,14 @@ export default function Page() {
     return (
         <NextUIProvider>
             <AppContext>
+                <div className={`round-lg bg-[url("/login-bg.webp")] bg-cover bg-center bg-no-repeat h-[100vh] items-center justify-center flex`}>
+                    <div className="flex flex-col items-center">
+                        <div className="text-[#fff] text-8xl mb-16 type-writer flex item-center font-bold" ref={ref}>
+                            AGI-Git
+                        </div>
+                        <ConnectButton />
+                    </div>
+                </div>
             </AppContext>
         </NextUIProvider>
     );
